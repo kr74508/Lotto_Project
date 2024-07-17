5f18b634bb89fa14646ff3cf3ee5c45dbf09f6ab
swon0913 <wonjunn912@naver.com>
2024-07-17 15:40:40 +0900

    update join user

--- Lotto_Project/src/Test/Lotto_User.java
+++ Lotto_Project/src/Test/Lotto_User.java
@@ -5,36 +5,32 @@ public class Lotto_User { // 정보를 넘겨주기 위함
 	private int year;
 	private int month;
 	private int day;
+	private String address;
 	private String pwd;
 
-	public void User(String name, int year, int month, int day, String pwd) {
+	public void User(String name, int year, int month, int day, String address, String pwd) {
 		this.name = name;
 		this.year = year;
 		this.month = month;
 		this.day = day;
+		this.address = address;
 		this.pwd = pwd;
 	}
 
 	// Getter methods
-	public String getName() {
-		return name;
-	}
+	public String getName() {return name;}
+	public int getYear() {return year;}
+	public int getMonth() {return month;}
+	public int getDay() {return day;}
+	public String getAddress() {return address;}
+	public String getPwd() {return pwd;}
 
-	public int getYear() {
-		return year;
-	}
-
-	public int getMonth() {
-		return month;
-	}
-
-	public int getDay() {
-		return day;
-	}
-
-	public String getPwd() {
-		return pwd;
-	}
-	
 	// Setter methods 구현
+	public void setName(String name) {	this.name = name;}
+	public void setYear(int year) {this.year = year;}
+	public void setMonth(int month) {this.month = month;}
+	public void setDay(int day) {this.day = day;}
+	public void setAddress(String address) {this.address = address;}
+	public void setPwd(String pwd) {this.pwd = pwd;}
+
 }
--- Lotto_Project/src/Test/Lotto_join.java
+++ Lotto_Project/src/Test/Lotto_join.java
@@ -11,84 +11,83 @@ public class Lotto_join {
 		this.sc = sc_join;
 		startSignUp();
 	}
-  
-	private void startSignUp() { //회원가입 진행
+
+	private void startSignUp() { // 회원가입 진행
 		System.out.println("[INFO] 안녕하세요. 동행로또입니다.");
 		System.out.println("로또 구입을 위해선 회원가입이 필요합니다.");
 		System.out.println("회원가입을 진행하시겠습니까?");
 		System.out.println("[1] 예\t[2] 아니요");
 		// try 시작
-		for (int i = 0; i < 1; i++) {
-			System.out.print("\n입력 : ");
-			String answer = sc.next();
-
-			if (answer.equals("1") || answer.equals("예")) {
-				SignUp();
-				break;
-			} else if (answer.equals("2") || answer.equals("아니요")) {
-				System.out.println("\n[에러] 회원이 아니면 로또를 구매할 수 없습니다.");
-				System.exit(0); // return; 으로 변경
-			} else {
-				System.out.println("\n[에러] 잘못된 입력입니다. 다시 입력해주세요.");
+		try {
+			for (int i = 0; i < 1; i++) {
+				System.out.print("\n*입력 : ");
+				String answer = sc.next();
+				if (answer.equals("1") || answer.equals("예")) {
+					SignUp();
+					break;
+				} else if (answer.equals("2") || answer.equals("아니요")) {
+					System.out.println("\n[에러] 회원이 아니면 로또를 구매할 수 없습니다.");
+					return; // return; 으로 변경
+				} else {
+					System.out.println("\n[에러] 잘못된 입력입니다. 다시 입력해주세요.");
+				}
 			}
-
+		} catch (NullPointerException e) {
+            System.err.println("에러 : 입력 값이 Null 입니다.");
+		} catch (Exception e) {
+			System.out.println("\n[에러] 알 수 없는 오류가 발생했습니다.");
+			e.printStackTrace();
 		}
-		// try 종료 catch 시작
-	}
-
+	} // try 종료 catch 시작
 	private void SignUp() { // 정보입력
 		System.out.println("[INFO] 회원가입을 위해 아래 양식을 채워주세요\n");
-
-		System.out.print("이름 : ");
+		Lotto_User user = new Lotto_User();
+		System.out.print("*이름 : ");
 		String name = sc.next(); // 이름
-		// 입력된 이름 저장
 		// setName(name);
-		
+		user.setName(name);
 		// 생년월일
 		int year = 0;
 		while (true) {
-			System.out.print("태어난 연도: ");
+			System.out.print("*태어난 연도: ");
 			try {
 				year = sc.nextInt();
 				break;
 			} catch (InputMismatchException e) {
 				System.out.println("[에러] 잘못된 입력입니다. 숫자로 입력해주세요");
-				sc.next(); // 무한루프 방지
 			}
 		}
 		int month = 0;
 		while (true) {
-			System.out.print("태어난 월 :");
+			System.out.print("*태어난 월 :");
 			try {
 				month = sc.nextInt();
 				break;
 			} catch (InputMismatchException e) {
 				System.out.println("[에러] 잘못된 입력입니다. 숫자로 입력해주세요");
-				sc.next(); // 무한루프 방지
 			}
 		}
 		int day = 0;
 		while (true) {
-			System.out.print("태어난 일 :");
+			System.out.print("*태어난 일 :");
 			try {
 				day = sc.nextInt();
 				break;
 			} catch (InputMismatchException e) {
 				System.out.println("[에러] 잘못된 입력입니다. 숫자로 입력해주세요");
-				sc.next(); // 무한루프 방지
 			}
 		}
-
 		if (!isAdult(year, month, day)) {
 			System.out.println("[에러] 미성년자는 로또를 구매할 수 없습니다.");
 			return;
 		}
-
-		System.out.print("주소 : "); // 주소
+		System.out.print("*주소 : "); // 주소
 		String address = sc.next();
+		user.setAddress(address);
 
-		System.out.print("비밀번호 : "); // 비밀번호
+		System.out.print("*비밀번호 : "); // 비밀번호
 		String pwd = sc.next();
+		user.setPwd(pwd);
 
 		System.out.println("[INFO] 회원가입이 완료 되었습니다.");
 	}
